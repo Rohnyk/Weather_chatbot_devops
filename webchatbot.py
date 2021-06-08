c4603a7e548801487db68dcbc40a804602cc80d3
Rohnyk <rohnyk1998@gmail.com>
2021-06-09 01:43:05 +0530

    third commit

--- webchatbot.py
+++ webchatbot.py
@@ -5,17 +5,20 @@ import json
 app = Flask(__name__, template_folder= 'Templates')
 context_set = ""
 
-@app.route('/', methods = ['POST', 'GET'])
+@app.route('/')
 def index():
-    if request.method == 'GET':
-        val = str(request.args.get('text'))
-        data = json.dumps({"sender": "Rasa","message": val})
-        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-        res = requests.post('http://127.0.0.1:5005/webhooks/rest/webhook', data= data, headers = headers)
-        res = res.json()
-        print(res)
-        val = res[0]['text']
-        return render_template('index.html', val=val)
+  return render_template('index.html')
+
+@app.route("/get")
+def get_bot_response():    
+  val = str(request.args.get('msg'))
+  data = json.dumps({"message": val})
+  headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+  res = requests.post('http://127.0.0.1:5005/webhooks/rest/webhook', data= data, headers = headers)
+  res = res.json()
+  print(res)
+  val = res[0]['text']
+  return str(val) 
 
 if __name__ == '__main__':
   app.run(debug=True)
